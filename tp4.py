59208121c572cb9ff1bcda4ec0678190526aaad5
amansil3 <amansil3@rosario.gov.ar>
2020-09-22 21:29:47 -0300

    Corregidas sintaxis propias de Python 2 para reconvertir el trabajo a Python 3

--- tp4.py
+++ tp4.py
@@ -2,49 +2,48 @@
 ingresados por el usuario'''
 
 #mensaje de bienvenida
-print "Bienvenido, ingrese 3 numeros distintos"
+print ("Bienvenido, ingrese 3 numeros distintos")
 bucle = True
 while (bucle == True):
     #solicito que ingrese los numeros
-    a = raw_input("Ingrese el primer numero: ")
-    b = raw_input("Ingrese el segundo numero: ")
-    c = raw_input("Ingrese el tercer numero: ")
-
-#valido que sean numeros y ejecuto el bucle hasta que ingrese numeros
-try:
-    float(a)
-    float(b)
-    float(c)
-except ValueError:
-    print "No ingresaste un numero!"
-else:
-#si es numero y no son distintos los comparo entre si
-    if (a!=b and b!=c and c!=a):
-        if (a>b):
-            if (b>c):
-                print "El menor es ", c,", el siguiente es", b,"y el mayor es ", a
-                bucle=False
+    a = input("Ingrese el primer numero: ")
+    b = input("Ingrese el segundo numero: ")
+    c = input("Ingrese el tercer numero: ")
+    #valido que sean numeros y ejecuto el bucle hasta que ingrese numeros
+    try:
+        float(a)
+        float(b)
+        float(c)
+    except ValueError:
+        print ("No ingresaste un numero!")
+    else:
+    #si es numero y no son distintos los comparo entre si
+        if (a!=b and b!=c and c!=a):
+            if (a>b):
+                if (b>c):
+                    print ("El menor es ", c,", el siguiente es", b,"y el mayor es ", a)
+                    bucle=False
+                else:
+                    if (a>c):
+                        print ("El menor es ", b,", el siguiente es", c,"y el mayor es ", a)
+                        bucle=False
+                    else:
+                        print ("El menor es ", b,", el siguiente es", a,"y el mayor es ", c)
+                        bucle=False
             else:
                 if (a>c):
-                    print "El menor es ", b,", el siguiente es", c,"y el mayor es ", a
+                    print ("El menor es ", c,", el siguiente es", a,"y el mayor es ", b)
                     bucle=False
                 else:
-                    print "El menor es ", b,", el siguiente es", a,"y el mayor es ", c
-                    bucle=False
+                    if (b>c):
+                        print ("El menor es ", a,", el siguiente es", c,"y el mayor es ", b)
+                        bucle=False
+                    else:
+                        print ("El menor es ", a,", el siguiente es", b,"y el mayor es ", c)
+                        bucle=False
+        #si son numeros iguales ejecuto el bucle hasta que ingrese numeros distintos
         else:
-            if (a>c):
-                print "El menor es ", c,", el siguiente es", a,"y el mayor es ", b
-                bucle=False
-            else:
-                if (b>c):
-                print "El menor es ", a,", el siguiente es", c,"y el mayor es ", b
-                bucle=False
-                else:
-                    print "El menor es ", a,", el siguiente es", b,"y el mayor es ", c
-                    bucle=False
-    #si son numeros iguales ejecuto el bucle hasta que ingrese numeros distintos
-    else:
-    print "Intentelo nuevamente, los numeros ingresados son iguales"
-    bucle=True
+            print ("Intentelo nuevamente, los numeros ingresados son iguales")
+            bucle=True
 
 
